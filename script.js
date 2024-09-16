c7ee19c83f16654374e9b072bf01865ce3f5359b
EduardoBitt <eduardo.ilibio@aluno.fmpsc.edu.br>
2024-09-15 23:31:36 -0300

    adição de formatação do cnpj

--- script.js
+++ script.js
@@ -16,6 +16,25 @@ async function validarCNPJ(cnpj) {
     }
 }
 
+function formatarCNPJ(cnpj) {
+    // Remove tudo que não for número
+    cnpj = cnpj.replace(/\D/g, '');
+
+    // Adiciona a formatação do CNPJ: ##.###.###/####-##
+    cnpj = cnpj.replace(/^(\d{2})(\d)/, "$1.$2");
+    cnpj = cnpj.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+    cnpj = cnpj.replace(/\.(\d{3})(\d)/, ".$1/$2");
+    cnpj = cnpj.replace(/(\d{4})(\d)/, "$1-$2");
+
+    return cnpj;
+}
+
+const cnpjInput = document.getElementById('cnpjInput');
+
+cnpjInput.addEventListener('input', function () {
+    this.value = formatarCNPJ(this.value);
+});
+
 // Evento de envio do formulário
 const form = document.getElementById('cnpjForm');
 form.addEventListener('submit', async function (event) {
