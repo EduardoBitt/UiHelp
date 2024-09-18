874f5db58c9b0b7e7be756dc4dc28c3d157bc298
EduardoBitt <eduardo.ilibio@aluno.fmpsc.edu.br>
2024-09-17 23:10:17 -0300

    Desenvolvimento do menu lateral

--- map.js
+++ map.js
@@ -4,3 +4,16 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+const hamburger = document.getElementById('hamburger');
+const closebtn = document.getElementById('closebtn');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('open');
+});
+
+closebtn.addEventListener('click', () => {
+    menu.classList.remove('open');
+});
