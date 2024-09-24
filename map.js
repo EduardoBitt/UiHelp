a5110be0a5465590291c841856c0aeb18a98334b
EduardoBitt <eduardo.ilibio@aluno.fmpsc.edu.br>
2024-09-23 21:52:58 -0300

    Implementação do pop-up no mapa

--- map.js
+++ map.js
@@ -1,5 +1,30 @@
 var map = L.map('map').setView([-27.63770377332425, -48.651619822297285], 10);
 
+// Define o ícone padrão do Leaflet com a cor verde
+var greenIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png', // Ícone verde original
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png', // Sombra do ícone
+
+    iconSize: [25, 41], // Tamanho do ícone
+    iconAnchor: [12, 41], // Ponto onde o ícone estará no mapa
+    popupAnchor: [1, -34], // Ponto de ancoragem do popup relativo ao ícone
+    shadowSize: [41, 41] // Tamanho da sombra
+});
+
+// Adiciona o marcador ao mapa
+var marker = L.marker([-27.63770377332425, -48.651619822297285], {icon: greenIcon}).addTo(map);
+
+marker.bindPopup(`
+    <div class="popup">
+        <h3>Faculdade Municipal de Palhoça</h3>
+        <p>Endereço: R. João Pereira dos Santos, 99 - Pte. do Imaruim, Palhoça - SC, 88130-475</p>
+        <p>Horário de Funcionamento: 08:00–22:00</p>
+        <p>Visite nosso site: <a href="http://fmpsc.edu.br/" target="_blank">Clique aqui</a></p>
+        <a class="instagram" href="https://www.instagram.com/faculdademunicipaldepalhoca/" target="_blank"><img src="img/instagram.png" style="width:30px;height:auto;"></a>
+        <img src="img/image.png" alt="Imagem do local" style="width:150px;height:auto;">
+    </div>
+`);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
